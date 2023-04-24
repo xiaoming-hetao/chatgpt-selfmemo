cd382a579ce280f1760f534672f7997c38b17e8d
xiaoming-hetao <xiaoming_li721@163.com>
2023-04-24 16:05:33 +0800

    feat: pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,11 @@ export default defineNuxtConfig({
     head: {
       title: '情绪窝',
       link: [
-        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' }
-      ]
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
+        },
+      ],
     },
   },
   css: [
@@ -14,10 +17,30 @@ export default defineNuxtConfig({
     '~/assets/icon/iconfont.css',
   ],
   modules: ['@pinia/nuxt', '@element-plus/nuxt'],
+  buildModules: ['@nuxtjs/pwa'],
   proxy: {
     '/api': {
       target: 'http://localhost:3000',
       pathRewrite: { '^/api': '' },
+    },
+  },
+  pwa: {
+    meta: {
+      title: '情绪窝',
+      author: 'xiaoming-hetao',
+    },
+    manifest: {
+      name: '情绪窝',
+      short_name: '情绪窝',
+      display: 'standalone',
+      start_url: './pages/hollow/index.vue',
+      useWebmanifestExtension: false,
+      description:
+        '一个智能的树洞，记录心情，支持以特定的角色发送心情给 AI，AI 会以设定的角色回复你，这个角色可以是朋友、家人、恋人等等',
+    },
+    icon: {
+      source: '~/static/desktop.png',
+      fileName: 'desktop.png',
     },
   },
 })
